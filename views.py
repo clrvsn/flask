dd18da3b8ed0270ec0292dd2f1d03b11c340a570
Martin Keefe <martin@westwood-software.co.uk>
2015-02-17 13:01:59 +0000

    Added tooltips and live data to byprog view.

--- views.py
+++ views.py
@@ -11,12 +11,25 @@
 
 from flask import render_template
 from app import app
+import models
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
-
+#def hello_world():
+#    return 'Hello from Flask!'
+#
 @app.route('/byprog')
 def byprog():
     return render_template("byprog.html",
                            title='Initiatives by Program')
+
+@app.route('/byprog/init.csv')
+def byprog_init_csv():
+    inits = models.Initiative.query.all()
+    fields = ['id','name','state','start','end','type','category','program','function','byprog_col','byprog_row','byprog_txt']
+    csv = ','.join(fields) + '\n'
+    csv += '\n'.join([','.join([str(getattr(init,name) or '') for name in fields]) for init in inits])
+    return csv
+
+
+if __name__ == '__main__':
+    pass
